8326baaf27851e85b5153edb4af2f76941589956
Kaylin-Ann Packareysammy <kaylin@growthtechdev.co.za>
2023-08-21 15:34:01 +0200

    End Of Section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>().ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+        .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfbirth.CalculateAge()));
+        CreateMap<Photo, PhotoDto>();
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
+using System.Data;
 using API;
+using API.Data;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +26,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An Error occured during the migration");
+}
 
 app.Run();
